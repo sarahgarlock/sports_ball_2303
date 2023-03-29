c6634ad05d1d6f3df28187734da0a344bec2fb7c
Sarah Garlock <garlock.sarah10@gmail.com>
2023-03-29 09:45:51 -0600

    Fixes spacing

--- lib/player.rb
+++ lib/player.rb
@@ -1,6 +1,7 @@
 require 'pry'
 class Player
   attr_reader :name, :monthly_cost, :contract_length
+  
   def initialize(name, monthly_cost, contract_length)
     @name = name
     @monthly_cost = monthly_cost
