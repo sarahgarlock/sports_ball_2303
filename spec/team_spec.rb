a5ebf325d08599cbe986f6b6ad4dfab66e50233f
Sarah Garlock <garlock.sarah10@gmail.com>
2023-03-29 10:35:01 -0600

    Add tests

--- spec/team_spec.rb
+++ spec/team_spec.rb
@@ -33,5 +33,51 @@ RSpec.describe Team do
       expect(team.roster).to eq([player_1, player_2])
       expect(team.player_count).to eq 2
     end
+
+    it 'has a contract greater than two years' do
+      team = Team.new("Dodgers", "Los Angeles")
+
+      player_1 = Player.new("Michael Palledorous" , 1000000, 36)
+      player_2 = Player.new("Kenny DeNunez", 500000, 24)
+      player_3 = Player.new("Alan McClennan", 750000, 48)
+      player_4 = Player.new("Hamilton Porter", 100000, 12)
+
+      team.add_player(player_1)
+      team.add_player(player_2)
+      team.add_player(player_3)
+      team.add_player(player_4)
+
+      expect(team.long_term_players).to eq([player_1, player_3])
+    end
+
+    it 'has a contract shorter or equal to two years' do
+      team = Team.new("Dodgers", "Los Angeles")
+
+      player_1 = Player.new("Michael Palledorous" , 1000000, 36)
+      player_2 = Player.new("Kenny DeNunez", 500000, 24)
+      player_3 = Player.new("Alan McClennan", 750000, 48)
+      player_4 = Player.new("Hamilton Porter", 100000, 12)
+
+      team.add_player(player_1)
+      team.add_player(player_2)
+      team.add_player(player_3)
+      team.add_player(player_4)
+
+      expect(team.short_term_players).to eq([player_2, player_4])
+    end
+
+    it 'has a total team value' do
+      team = Team.new("Dodgers", "Los Angeles")
+
+      player_1 = Player.new("Michael Palledorous" , 1000000, 36)
+      player_2 = Player.new("Kenny DeNunez", 500000, 24)
+      player_3 = Player.new("Alan McClennan", 750000, 48)
+      player_4 = Player.new("Hamilton Porter", 100000, 12)
+
+      team.add_player(player_1)
+      team.add_player(player_2)
+      team.add_player(player_3)
+      team.add_player(player_4)
+    end
   end
 end
